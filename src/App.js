81829f0ade7b79ffd2533c90fa2d0662747a6af8
apoorva-19 <apoorva19.am@gmail.com>
2020-11-02 22:01:06 +0530

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
-import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
+import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+// import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
 
 function App() {
   return (
     <div className="container">
-      <Final />
+      <Setup />
     </div>
   );
 }
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h2>value:{secondValue}</h2> */}
+      <h1>{text || "John Doe"}</h1>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => setIsError(!isError)}
+      >
+        Toggle error
+      </button>
+      {/* {isError && <h1>Error...</h1>} */}
+      {isError ? (
+        <h2>There is an error...</h2>
+      ) : (
+        <div>
+          <h2>There is no error...</h2>
+        </div>
+      )}
+      {/* {text || <h1>Hello World</h1>}
+      {!text && <h1>Hello World</h1>} */}
+    </>
+  );
 };
 
 export default ShortCircuit;
